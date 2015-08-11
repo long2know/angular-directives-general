9c4025b6a3521a218bc20ad0097c5aa718b08048
Stephen Long <long2know@gmail.com>
2015-08-11 13:22:35 -0400

    Added isState directive

---
+++ demo/isState/script.js
@@ -0,0 +1,45 @@
+(function () {
+    angular.module('myApp.controllers', []);
+    angular.module('myApp.services', ['ngResource', 'ngAnimate']);
+
+    var myApp = angular.module('myApp', [
+        'myApp.controllers',
+        'myApp.services',
+        'long2know',
+        'ngSanitize',
+        'ui.bootstrap',
+        'ui.router',
+        'ui']);
+
+    var myController = function ($scope, $timeout, $animate, $log) {
+        var vm = this,
+            initItems = function () {
+                vm.items = [];
+                vm.items2 = [];
+                for (var i = 0; i < 10; i++) {
+                    vm.items.push({ name: "Item1 " + i.toString() });
+                    vm.items2.push({ name: "Item2 " + i.toString() });
+                }
+            },
+            init = function () {
+                vm.timeout = 3000;
+                $timeout(function () { setTimeout(initItems(), 2000) }, 0);
+            };
+
+        vm.addItem = function () {
+            vm.items.push({ name: "Item " + vm.items.length.toString() });
+        };
+
+        vm.itemListClick = function (item) {
+            $timeout(function () { item.isSelected = false; }, vm.timeout);
+        };
+
+        init();
+    };
+
+    myController.$inject = ['$scope', '$timeout', '$animate', '$log'];
+    angular.module('myApp.controllers')
+        .controller('myCtrl', myController);
+
+    myApp.run(['$log', function ($log) { $log.log("Start."); }]);
+})()
---
+++ src/isState.js
@@ -0,0 +1,57 @@
+(function () {
+    var long2know;
+    try {
+        long2know = angular.module("long2know")
+    } catch (err) {
+        long2know = null;
+    }
+
+    if (!long2know) {
+        angular.module('long2know.services', ['ngResource', 'ngAnimate']);
+        angular.module('long2know.controllers', []);
+        angular.module('long2know.directives', []);
+        angular.module('long2know',
+            [
+                'long2know.services',
+                'long2know.controllers',
+                'long2know.directives'
+            ]);
+    }
+
+    var isStateDirective = function ($animate, $timeout) {
+        var directive = {
+            restrict: 'A',
+            scope: {
+                isState: '=',
+                isStateClass: '@',
+                isStateTimeout: '@'
+            },
+            link: function (scope, element, attrs) {
+                var
+                    className = angular.isDefined(attrs.isStateClass) ? attrs.isStateClass : 'is-state',
+                    timeout = angular.isDefined(attrs.isStateTimeout) ? scope.isStateTimeout : 2000,
+                    startAnimation = function () {
+                        if (scope.isState) {
+                            $animate.addClass(element, className);
+                            $timeout(function () {
+                                scope.isState = false;
+                                $animate.removeClass(element, className);
+                            }, timeout);
+                        } else if (scope.isState === false) {
+                            $animate.removeClass(element, className);
+                        }
+                    };
+
+                // Watch the attribute to toggle
+                scope.$watch('isState', function () {
+                    startAnimation();
+                });
+            }
+        };
+        return directive;
+    };
+
+    isStateDirective.$inject = ['$animate', '$timeout'];
+    angular.module("long2know.directives")
+        .directive('isState', isStateDirective);
+})()
